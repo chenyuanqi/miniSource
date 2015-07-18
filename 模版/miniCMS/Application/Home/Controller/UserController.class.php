a648f9ce30a2117039efde7c72a6997970389a25
chenyuanqi <chenyuanqi90s@163.com>
2015-07-18 16:58:06 +0800

    添加 Model 处理，及 Controller 的拼装

--- 模版/miniCMS/Application/Home/Controller/UserController.class.php
+++ 模版/miniCMS/Application/Home/Controller/UserController.class.php
@@ -27,6 +27,33 @@ class UserController extends CommonController {
 
 
     /**
+     * 用户添加处理
+     * @author cyq <chenyuanqi90s@163.com>
+     */
+    public function userAddAction(){
+        if (IS_POST) {
+            // 拼装数组
+            $userArr = array(
+                'user_name'  => I('post.name', '', 'htmlspecialchars'),
+                'password'   => I('post.pwd', '', 'htmlspecialchars'),
+                'create_time'=> date('Y-m-d H:i:s')
+            );
+
+            // Model 验证
+            D('User')->create($userArr);
+
+            // 开始处理，并返回处理结果
+            if(D('User')->addUser($userArr)) {
+                $this->ajaxReturn('success');
+            } else{
+                $this->ajaxReturn('error');
+            }
+
+        }
+    }
+
+
+    /**
      * 用户详情
      * @author cyq <chenyuanqi90s@163.com>
      */
---
+++ 模版/miniCMS/Application/Home/Model/UserModel.class.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Home\Model;
+
+
+use Think\Model;
+
+class UserModel extends Model {   
+    protected $_validate = array(
+            array("phone","require","请输入手机号码",1,"",1),//验证手机号是否填写
+            array("phone","/^(1[3|5|8])[\d]{9}$/","手机号码不正确",2,"regex",1),//验证手机号码是否正确
+            array("phone","","手机号码已经被注册",1,"unique",1),// 验证手机号码是否被注册
+            array("telephone","/^((0\d{2,3}-\d{7,8})|(1[3584]\d{9}))$/","固定电话号码不正确",2,"regex",1),//验证固定电话是否正确
+            array("email","/^\w+@[0-9a-z-]+\.[a-z]+(\w+)?$/i","邮箱不正确",2,"regex",1),//验证邮箱是否正确
+            array("identity","/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4}$/","身份证号码不正确",2,"regex",3),//验证身份证是否正确
+            array("age","number","年龄不正确",2,"",3),//验证年龄是否合法;
+    );
+    
+
+	/**
+     * 用户添加
+     * @param  *addData (Array)  添加的数组数据
+     * @author cyq <chenyuanqi90s@163.com>
+     **/
+    protected function addUser($addData) {
+        $result = M('User')->add($addData);
+        return $result;
+    }
+
+
+    /**
+     * 用户编辑
+     * @param  *addData (Array)  编辑的数组数据
+     * @author cyq <chenyuanqi90s@163.com>
+     **/
+    protected function editUser($editData) {
+        $result = M('User')->save($editData);
+        return $result;
+    }
+
+
+    /**
+     * 用户删除
+     * @param  *id (Int/String)  要删除的记录 ID / id 以英文逗号 (,) 拼接的数组 =>删除多条记录
+     * @author cyq <chenyuanqi90s@163.com>
+     **/
+    protected function delUser($id) {
+        $result = M('User')->delete($id);
+        return $result;
+    }
+
+
+    /**
+     * 用户查询
+     * @param  *id (Int)  要查询的记录 ID
+     * @param  field (String) 要获取的用户字段
+     * @author cyq <chenyuanqi90s@163.com>
+     **/
+    protected function getUserInfo($id, $field = null) {
+        $result = M('User')->find($id);
+        return is_null($field) ? $result : $result[$field];
+    }
+
+
+     /**
+     * 用户头像上传
+     * @param  *id (Int)  要查询的记录 ID
+     * @param  field (String) 要获取的用户字段
+     * @author cyq <chenyuanqi90s@163.com>
+     **/
+    protected function uploadHeadImg($fileName) {
+        if (!empty($_FILES) && 0 === $_FILES[$fileName]["error"]) {
+            $uploadObj = new \Think\Upload();// 实例化上传类    
+            $uploadObj->maxSize   =     $maxSize;// 设置附件上传大小    
+            $uploadObj->exts      =     array('jpg', 'gif', 'png', 'jpeg', 'mp4', 'flv');// 设置附件上传类型
+            $uploadObj->allowTypes=    array('image/jpeg','image/png','image/gif','image/wbmp', 'video/mp4',  "video/flv");// 指定文件类型    
+            $uploadObj->rootPath  =     './Upload/';
+            $uploadObj->savePath  =     $savepath; // 设置附件上传目录
+            
+            // 上传文件     
+            $info =   $uploadObj->upload();
+
+            if(!$info) {
+                // 上传报错响应, 与路径对比，无 ‘/’   
+                return $uploadObj->getError();    
+            } else {     
+                // 开始上传,移动
+                $path = substr($info[$fileName]['savepath'],strpos($info[$fileName]['savepath'],'/'));
+                return $path.$info[$fileName]['savename'];      
+            }
+        }
+    }
+
+
+    /**
+     * 判断是否报错/返回路径
+     * @author cyq <chenyuanqi90s@163.com>
+     **/
+    protected function isPath($path) {
+        $result = strrpos($path, '/');
+        return $result ? true : false;
+    }
+
+
+			
+ }
+
